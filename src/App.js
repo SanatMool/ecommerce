2518f66bbb6f2490ac0c0029258bda9ec145cc31
Sanat Mool <sanat.mool17@gmail.com>
2020-11-05 12:06:25 +0545

    created header component for navigation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+      <Header />
       {/* switch will match slash first and not render anything after it  */}
       <Switch>
         <Route exact path="/" component={HomePage} />
