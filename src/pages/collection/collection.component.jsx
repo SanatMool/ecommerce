da49489f6383396b690bea4025fe27a4020ce5b3
Sanat Mool <sanat.mool17@gmail.com>
2020-11-11 14:45:57 +0545

    moved data into reducer

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,11 +8,20 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection">
-    <h2>Collection PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 // this is necessary because unlike other selectors,
 // this selector needs a part of the state depending on the URL parameter
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,14 +5,6 @@ import memoize from "lodash.memoize";
 
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -20,12 +12,16 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 // memoize does the same idea of memoization as reselect does for the selectors
 // here we are memoizing the return of our function which return our selector
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   )
 );
